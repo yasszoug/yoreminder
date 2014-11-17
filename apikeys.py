461cbeaf826de2550d8e3d78f0938f6659b3227c
yasszoug <ysnowtech@gmail.com>
2014-11-17 10:34:37 +0000

    Update apikeys.py

--- apikeys.py
+++ apikeys.py
@@ -1,8 +1,8 @@
 # This is the apikeys.py
 
 APIDATA = {
-           YOINANHOUR_CALLBACK: (YOINANHOUR, 60*60),
-           YOIN30MIN_CALLBACK: (YOIN30MIN,  30*60),
-           YOIN15MIN_CALLBACK: (YOIN15MIN,  15*60),
-           YOIN5MIN_CALLBACK: (YOIN5MIN, 5*60),
+           YOINANHOUR_CALLBACK: (edf21b7a-466e-4e95-9419-a302c351ea77, 60*60),
+           YOIN30MIN_CALLBACK: (dba8b4ea-3cbd-4a00-ac68-9af3950cb5e8,  30*60),
+           YOIN15MIN_CALLBACK: (c90b60a1-7de7-44f6-ae94-38153e2163e9,  15*60),
+           YOIN5MIN_CALLBACK: (f1394fb8-b32b-4270-8a80-c9a00b522952, 5*60),
      
